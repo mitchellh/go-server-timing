ef8b2ca6be017673f4053d73b9cf3ba650182260
Eyal <posener@gmail.com>
2018-02-23 04:17:22 +0200

    fix timing duration parsing of non-integer values

--- header.go
+++ header.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 	"regexp"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -59,11 +58,8 @@ func ParseHeader(input string) (*Header, error) {
 		// is what modern browsers are treating it as. If the parsing of
 		// an integer fails, the set value remains in the Extra field.
 		if v, ok := m.Extra[paramNameDur]; ok {
-			intv, err := strconv.Atoi(v)
-			if err == nil {
-				m.Duration = time.Duration(intv) * time.Millisecond
-				delete(m.Extra, paramNameDur)
-			}
+			m.Duration, _ = time.ParseDuration(v + "ms")
+			delete(m.Extra, paramNameDur)
 		}
 
 		metrics = append(metrics, &m)
